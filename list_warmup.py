8a7323ff14b61b8fe14bf36307592e9e7a262a8f
jc725951 <aaishwinderkaur@my.jcu.edu.au>
2020-04-20 10:01:50 +1000

    "Add list warmup solution to prac_4"

---
+++ list_warmup.py
@@ -0,0 +1,11 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# Change the first element of numbers to "ten"
+numbers[0] = "ten"
+print(numbers)
+
+numbers[-1] = 1
+print(numbers)
+
+print(numbers[2:])
+
+print(9 in numbers)
